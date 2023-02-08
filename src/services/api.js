474ea793b60c8d1d481e0227d6a1a81a6a30dfe3
OlexTS <sashokseven@gmail.com>
2023-02-09 00:49:50 +0200

    created fetches

---
+++ src/services/api.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'bb60805928e986df13f7a98c4acc6577';
+
+export const fetchTrendingMovies = async () => {
+    const response = await axios.get(`trending/get-trending/movie/day?api_key=${API_KEY}`);
+
+    return response.data.results;
+}
+
+export const fetchSearchQuery = async query => {
+    const response = await axios.get(`search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`);
+
+    return response.data.results;
+}
+
+export const fetchMovieDetails = async id => {
+    const response = await axios.get(`movie/${id}?api_key=${API_KEY}&language=en-US`);
+
+    return response.data;
+}
+
+export const fetchMovieCredits = async id => {
+    const response = await axios.get(`movie/${id}/credits?api_key=${API_KEY}&language=en-US`);
+
+    return response.data.cast;
+}
+
+export const fetchMovieReviews = async id => {
+    const response = await axios.get(`movie/${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`);
+
+    return response.data.results;
+}
